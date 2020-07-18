7b806e87cf22467a316c5514e53d692edf3522e5
Bramvd3 <bram.vandendriessche@realimpactanalytics.com>
2020-07-18 12:12:58 +0200

    add_to_cart functionality

--- core/models.py
+++ core/models.py
@@ -25,19 +25,31 @@ class Item(models.Model):
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
+    description = models.TextField()
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("core:product-page", kwargs={"slug": self.slug})
+        return reverse("core:product-page", kwargs={
+            "slug": self.slug
+        })
+
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
 
 
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.quantity} of {self.item.title}"
 
 # kind of shopping cart
 
@@ -51,4 +63,4 @@ class Order(models.Model):
     ordered = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return self.user.username
