08856af3ad449962dab617f74820c5536db0adce
Bramvd3 <bram.vandendriessche@realimpactanalytics.com>
2020-07-21 17:16:56 +0200

    collectstatic

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from django_countries.fields import CountryField
 
+PAYMENT_CHOICES = (
+    ('S', 'Stripe'),
+    ('P', 'PayPal')
+)
+
 
 class CheckoutForm(forms.Form):
     first_name = forms.CharField()
@@ -12,4 +17,5 @@ class CheckoutForm(forms.Form):
     zip = forms.CharField()
     same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
     save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect())
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect(), choices=PAYMENT_CHOICES)
